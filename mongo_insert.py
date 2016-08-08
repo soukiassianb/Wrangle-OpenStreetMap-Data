34c66280893fb0e7f1bee221f564b0ff783d3c94
soukiab <soukiassianb@gmail.com>
2016-08-08 12:24:56 +0200

    insert to mongo db

---
+++ mongo_insert.py
@@ -0,0 +1,15 @@
+import json
+from pymongo import MongoClient
+
+def insert_data(data, db):
+    for row in data:
+        db.larochelle.insert(row)
+
+if __name__ == "__main__":
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.larochelle
+
+    with open('processed-la-rochelle_france.json') as f:
+        data = json.loads(f.read())
+        insert_data(data, db)
+        print db.larochelle.find_one()
